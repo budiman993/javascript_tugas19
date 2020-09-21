3abea5ec2db4d401a6e483f0e42000b80e74e487
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-21 11:44:35 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,15 @@
+var kal1 = "Semangat..."
+console.log(kal1.repeat(5))
+console.log(" ")
+
+var bintang = "*****"
+
+console.log(bintang.repeat(1))
+console.log(bintang.repeat(4))
+console.log(bintang.repeat(7))
+console.log(bintang.repeat(10))
+console.log(bintang.repeat(13))
+console.log(bintang.repeat(10))
+console.log(bintang.repeat(7))
+console.log(bintang.repeat(4))
+console.log(bintang.repeat(1))
